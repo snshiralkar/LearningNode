65b7e0844fd7b85e813ab6931b2b6e81435053ca
shrikrushna <snshiralkar25@gmail.com>
2024-09-13 16:18:18 +0530

    adding new information

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 const fs = require("fs");
+
+//to create file
 fs.writeFile("read.txt", "welcome", (err) => {
   console.log(err);
 });
 
+//to change something in file
 fs.appendFile("read.txt", " to my home", (err) => {
   console.log(err);
 });
 
+//to read file
 fs.readFile("read.txt", "utf-8", (err, data) => {
   console.log(data);
   console.log(err);
 });
 
+//rename file
 fs.rename("read.txt", "module.txt", (err) => {
   console.log(err);
 });
 
+// to delete file
 fs.unlink("module.txt", (err) => {
   console.log(err);
 });
+
+//creating folder
 
 fs.mkdir("ram", (err) => {
   console.log(err);
 });
 
+//deleting folder
 fs.rmdir("ram", () => {});
